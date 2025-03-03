e01a09b897eed1eba074dc166e539b6fa8552385
kieferiacaruso <115434647+kieferiacaruso@users.noreply.github.com>
2025-03-02 22:38:37 -0500

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,25 +1,11 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: { type: String, unique: true },
-  password: String,
-  role: { type: String, enum: ['employee', 'company'], required: true }
+const UserSchema = new mongoose.Schema({
+    googleId: { type: String, unique: true, required: true },
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    imageUrl: { type: String },
+    createdAt: { type: Date, default: Date.now }
 });
 
-// Hash password before saving
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-// Compare password method
-userSchema.methods.comparePassword = function(password) {
-  return bcrypt.compare(password, this.password);
-};
-
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+module.exports = mongoose.model('User', UserSchema);
