31f4ae1c5a8ca68a81e864000de4a14b54c9cc57
kieferiacaruso <115434647+kieferiacaruso@users.noreply.github.com>
2025-03-02 23:09:15 -0500

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -5,6 +5,9 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     imageUrl: { type: String },
+    userType: { type: String, enum: ['employee', 'company'], required: true },
+    companyName: { type: String }, // For company users
+    position: { type: String }, // For employee users
     createdAt: { type: Date, default: Date.now }
 });
 
